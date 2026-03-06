d4f1df343a8a649dc06b2ec39a3ec3addf3e84ba
rarespapacioc12 <rares.papacioc9585@gmail.com>
2026-03-06 23:37:21 +0200

    Finished SD Lab1

--- AN 1/Semestrul 2/SD/Lab 1/main.cpp
+++ AN 1/Semestrul 2/SD/Lab 1/main.cpp
@@ -327,48 +327,271 @@
 //     S.pop();
 // }
 
-// 5.3.1 Problem 1
+// 5.3.1 Problem 1 + 5.3.2 Problem 2
 
-// TO-DO finish here
+// #include <iostream>
 
-#include <iostream>
+// struct Node{
+//     int data;
+//     Node* next, *prev;
 
-struct Node{
-    int data;
-    Node* next, *prev;
-};
+//     Node(int value){
+//         data = value;
+//         prev = nullptr;
+//         next = nullptr;
+//     }
+// };
 
-class LinkedList{
-    Node* head, *tail;
+// class LinkedList{
+// public:
+//     Node* head;
+//     Node* tail;
+// public:
+//     LinkedList(){
+//         head = tail = nullptr;
+//     }
 
-public:
-    LinkedList(){
-        head->data = 0;
-        head->next = head->prev = nullptr;
-        tail = head;
-    }
+//     void push_back(int x){
+//         if(head == nullptr){
+//             head = new Node(x);
+//             head->next = tail;
+//         }
+//         else if(tail == nullptr){
+//             tail = new Node(x);
+//             tail->prev = head;
+//             head->next = tail;
+//         }
+//         else{
+//             Node* t = new Node(x);
+//             tail->next = t;
+//             t->prev = tail;
+//             tail = t;
+//         }
+//     }
 
-    void push_back(int x){
-        if(head->next == nullptr){
-            tail = new Node;
-            head->data = x;
-            head->next = tail;
-        }
-        else{
-            Node* t = new Node;
-            t->data = x;
-            tail->next = t;
-            tail = t;
-        }
-    }
-    void pop_back(){
+//     void pop_back(){
+//         if(head == nullptr){
+//             std::cout << "Empty list!\n";
+//             return;
+//         }
+//         else if(tail == nullptr){
+//             delete head;
+//             head = tail = nullptr;
+//         }
+//         else if(head->next == tail){
+//             delete tail;
+//             tail = nullptr;
+//             head->next = nullptr;
+//         }
+//         else{
+//             Node* t = tail->prev;
+//             t->next = nullptr;
+//             delete tail;
+//             tail = t;
+//         }
+//     }
 
-    }
-};
+//     void push_front(int x){
+//         if(head == nullptr){
+//             head = new Node(x);
+//             head->next = tail;
+//         }
+//         else if(tail == nullptr){
+//             tail = head;
+//             head = new Node(x);
+//             tail->prev = head;
+//             head->next = tail;
+//         }
+//         else{
+//             Node* t = new Node(x);
+//             t->next = head;
+//             head->prev = t;
+//             head = t;
+//         }
+//     }
 
-int main(){
+//     void pop_front(){
+//         if(head == nullptr){
+//             std::cout << "Empty list!\n";
+//             return;
+//         }
+//         else if(tail == nullptr){
+//             delete head;
+//             head = tail = nullptr;
+//         }
+//         else if(head->next == tail){
+//             delete head;
+//             head = tail;
+//             tail = nullptr;
+//             head->prev = nullptr;
+//         }
+//         else{
+//             Node* t = head->next;
+//             t->prev = nullptr;
+//             delete head;
+//             head = t;
+//         }
+//     }
 
-}
+//     Node* find_by_index(int index){
+//         if(index < 0){
+//             std::cout << "Index out of bounds!\n";
+//             return nullptr;
+//         }
+//         Node* t = head;
+//         int i = 0;
+//         while(t != nullptr and i < index){
+//             ++i;
+//             t = t->next;
+//         }
+//         if(t == nullptr){
+//             std::cout << "Index out of bounds!\n";
+//             return nullptr;
+//         }
+//         else{
+//             return t;
+//         }
+//     }
+
+//     Node* find_by_value(int value){
+//         Node* t = head;
+//         while(t != nullptr and t->data != value){
+//             t = t->next;
+//         }
+//         if(t == nullptr){
+//             std::cout << "Value not in list!\n";
+//             return nullptr;
+//         }
+//         else{
+//             return t;
+//         }
+//     }
+
+//     void insert_at(int value, int poz){
+//         if(poz < 0){
+//             std::cout << "Position out of bounds!\n";
+//             return;
+//         }
+//         Node* t = head;
+//         int i = 0;
+//         while(t != nullptr and i < poz){
+//             ++i;
+//             t = t->next;
+//         }
+//         if(t == nullptr){
+//             if(i == poz){
+//                 push_back(value);
+//             }
+//             else{
+//                 std::cout << "Position out of bounds!\n";
+//             }
+//             return;
+//         }
+//         Node* u = new Node(value);
+//         u->prev = t->prev;
+//         u->next = t;
+//         t->prev = u;
+//         if(u->prev)
+//             u->prev->next = u;
+//         else
+//             head = u;
+//     }
+
+//     void erase_at(int poz){
+//         if(poz < 0){
+//             std::cout << "Position out of bounds!\n";
+//             return;
+//         }
+//         Node* t = head;
+//         int i = 0;
+//         while(t != nullptr and i < poz){
+//             ++i;
+//             t = t->next;
+//         }
+//         if(t == nullptr){
+//             std::cout << "Position out of bounds!\n";
+//             return;
+//         }
+//         Node* a = t->prev;
+//         Node* b = t->next;
+//         if(a == nullptr){
+//             pop_front();
+//         }
+//         else if(b == nullptr){
+//             pop_back();
+//         }
+//         else{
+//             a->next = b;
+//             b->prev = a;
+//             delete t;
+//         }
+//     }
+
+//     void detect_cycle(){
+//         Node* slow = head;
+//         Node* fast = head;
+
+//         while(fast != nullptr && fast->next != nullptr){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow == fast){
+//                 slow = head;
+//                 while(slow != fast){
+//                     slow = slow->next;
+//                     fast = fast->next;
+//                 }
+//                 //now slow points at the starting point of the cycle
+//                 std::cout << "Start node of cycle has value " << slow->data;
+//                 Node* t = slow->next;
+//                 int length = 1; //acounting for start
+//                 while(t != slow){
+//                     t = t->next;
+//                     ++length; // basically accounting for the previous t
+//                 }
+//                 std::cout << " and the cycle has length " << length << '\n';
+//                 return;
+//             }
+//         }
+
+//         std::cout << "No cycle found\n";
+//     }
+
+//     friend std::ostream& operator<<(std::ostream& os, const LinkedList& L){
+//         Node* t = L.head;
+//         if(t == nullptr)
+//             return os;
+//         while(t->next != nullptr){
+//             os << t->data << ' ';
+//             t = t->next;
+//         }
+//         os << t->data << '\n';
+//         return os;
+//     }
+
+//     ~LinkedList() {
+//         while(head != nullptr){
+//             pop_front();
+//         }
+//     }
+// };
+
+// LinkedList L;
+
+// int main(){
+//     L.push_back(3);
+//     L.push_back(2);
+//     L.push_back(1);
+//     std::cout << L;
+//     L.pop_back();
+//     std::cout << L;
+//     L.push_front(-1);
+//     L.push_front(-2);
+//     L.pop_front();
+//     std::cout << L;
+// }
+
+// 5.4.1 Problem 1 - Candies
+// Solution here: https://leetcode.com/problems/maximum-candies-allocated-to-k-children/submissions/1940028279/
 
 // 5.4.2 Problem 2
 
@@ -392,4 +615,47 @@ int main(){
 //     }
 
 //     std::cout << st;
+// }
+
+// 5.5.1 Problem 1 - LRU Cache
+
+// https://leetcode.com/problems/lru-cache/submissions/1940285105/
+
+// 5.5.2 Problem 2 - Find Any Duplicate
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// int findDuplicate(const vector<int>& v) {
+//     int slow = v[0], fast = v[v[0]];
+    
+//     while(slow != fast){
+//         slow = v[slow];
+//         fast = v[v[fast]];
+//     }
+    
+//     slow = 0;
+    
+//     while(slow != fast){
+//         slow = v[slow];
+//         fast = v[fast];
+//     }
+    
+//     return slow;
+// }
+
+// int main() {
+//     int N;
+//     cin >> N;
+    
+//     vector<int> v;
+//     for (int i = 0; i <= N; ++i) {
+//         int val;
+//         cin >> val;
+//         v.push_back(val);
+//     }
+    
+//     cout << findDuplicate(v) << "\n";
+//     return 0;
 // }
