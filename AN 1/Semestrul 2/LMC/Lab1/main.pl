1a4db0aeb225cdbf52d75c190ac26b345ee18a29
Rares Papacioc <rares.papacioc9585@gmail.com>
2026-03-09 21:27:20 +0200

    Finished labs 1 and 2 at LMC

--- AN 1/Semestrul 2/LMC/Lab1/main.pl
+++ AN 1/Semestrul 2/LMC/Lab1/main.pl
@@ -1,10 +1,63 @@
-% father(peter,meg).
+% Care din urm˘atoarele ¸siruri de caractere sunt constante ¸si care sunt
+% variabile ˆın Prolog?
+% • vINCENT - constanta
+% • Footmassage - variabila
+% • variable23 - constanta
+% • Variable2000 - variabila
+% • big_kahuna_burger - constanta
+% • ’big kahuna burger’ - constanta
+% • big kahuna burger - nici una, nici alta
+% • ’Jules’ - constanta
+% • _Jules - variabila
+% • ’_Jules’ - constanta
+
+% windy :- oslo.
+% norway :- oslo.
+% cold :- norway.
+% winterIsComing :- windy, cold.
+% oslo.
+
+% father(peter,meg). -> se numeste FAPT
 % father(peter,stewie).
 % mother(lois,meg).
 % mother(lois,stewie).
 % griffin(peter).
 % griffin(lois).
-% griffin(X) :- father(Y,X), griffin(Y).
+% griffin(X) :- father(Y,X), griffin(Y). -> se numeste REGULA
+
+% operatorul :- modeleaza implicatia logica <-
+% virgula modeleaza conjunctia ^
+% disjunctia v este modelata astfel:
+% p(X) :- a(X).
+% p(X) :- b(X).
+% p(X) :- c(X).
+% este echivalent cu p(X) = a(X) v b(X) v c(X)
+
+% culoare(albastru).
+% culoare(verde).
+% culoare(rosu).
+% culoare(galben).
+
+% vecin(X, Y) :-
+%     culoare(X),
+%     culoare(Y),
+%     X \== Y.
+
+% harta(RO, MD, UA, HU, SE, BU) :-
+%     vecin(RO, MD),
+%     vecin(RO, UA),
+%     vecin(RO, HU),
+%     vecin(RO, SE),
+%     vecin(RO, BU),
+%     vecin(MD, UA),
+%     vecin(UA, HU),
+%     vecin(HU, SE),
+%     vecin(SE, BU).
+
+% T = U reuseste daca exista o potrivire (termenii se unifica)
+% T \= U reuseste daca nu exista o potrivire
+% T == U reuseste daca termenii sunt identici
+% T \== U reuseste daca termenii nu sunt identici
 
 % bigger(elephant, horse).
 % bigger(horse, donkey).
@@ -98,22 +151,31 @@ uncle_of(Uncle, Person) :-
     brother_of(Uncle, Parent),
     parent(Parent, Person).
 
+ancestor_of(X, Y) :-
+    parent(X, Y).
+
+ancestor_of(X, Y) :-
+    parent(X, Z),
+    ancestor_of(Z, Y).
+
 not_parent(X, Y) :-
-    \+ parent(X, Y),
     male(X),
-    male(Y).
+    male(Y),
+    X \== Y,
+    \+ parent(X, Y).
 
 not_parent(X, Y) :-
-    \+ parent(X, Y),
     female(X),
-    male(Y).
+    male(Y),
+    \+ parent(X, Y).
 
 not_parent(X, Y) :-
-    \+ parent(X, Y),
     male(X),
-    female(Y).
+    female(Y),
+    \+ parent(X, Y).
 
 not_parent(X, Y) :-
-    \+ parent(X, Y),
     female(X),
-    female(Y).
+    female(Y),
+    X \== Y,
+    \+ parent(X, Y).
--- AN 1/Semestrul 2/LMC/Lab_Sem_2/main.pl
+++ AN 1/Semestrul 2/LMC/Lab_Sem_2/main.pl
@@ -1,6 +1,9 @@
+% Se aplica formula
 distance((X1, Y1), (X2, Y2), D) :-
     D is sqrt((X2 - X1)**2 + (Y2 - Y1) ** 2).
 
+
+% Asa faci memoizare aparent
 :- table fibo/2.
 
 fibo(0, 1).
@@ -14,16 +17,20 @@ fibo(Y, X) :-
     fibo(P2, B),
     X is A + B.
 
+% Definesc linia vida ca adevar
 line(0, Char).
 
+% Scriu linia caracter cu caracter
 line(X, Char) :-
     X > 0,
+    X1 is X - 1,
     write(Char),
-    NewX is X - 1,
-    line(NewX, Char).
+    line(X1, Char).
 
-rectangle(0, Y, Char).
+% Definesc dreptunghiul sub forma de linie ca adevar
+rectangle(0, Y, Char). 
 
+% Construiesc dreptunghiul linie cu linie
 rectangle(X, Y, Char) :-
     X > 0,
     line(Y, Char),
@@ -31,53 +38,74 @@ rectangle(X, Y, Char) :-
     X1 is X - 1,
     rectangle(X1, Y, Char).
 
-square(0, Char).
-square(X, Char) :-
-    rectangle(X, X, Char).
+square(X, Char) :- rectangle(X, X, Char).
 
+% X e in lista daca e capul ei (primul el)
 element_of(X, [X | _]).
+% SAU X e in lista daca nu e capul ei, dar se afla in coada listei (restul elementelor, fara primul)
 element_of(X, [_ | Tail]) :- element_of(X, Tail).
 
-concat_lists([], List, List).
+% Daca L1 = vid, atunci concatenarea lor e chiar L2
+concat_lists([], List, List). % concateneaza L1 cu L2 in L3
+
+% SAU L1 nu e vid, deci pun elementele din ele unul cate unul in L3 la inceput
 concat_lists([Elem | List1], List2, [Elem | List3]) :-
     concat_lists(List1, List2, List3).
 
-is_a(X):-
-    X == a.
-all_a([X]) :-
-    is_a(X).
+% Multimea vida respecta conditia
+all_a([]).
 
-all_a([X | Tail]) :-
-    is_a(X),
+% Verific daca Head e a si continui verificarea pentru restul listei.
+% A se vedea ca daca Head e variabila, i se atribuie valoarea a pentru a rezolva ecuatia practic
+all_a([Head | Tail]) :-
+    Head = a,
     all_a(Tail).
 
-is_b(X):-
-    X == b.
-all_b([X]) :-
-    is_b(X).
+% Analog all_a
+all_b([]).
 
-all_b([X | Tail]) :-
-    is_b(X),
+all_b([Head | Tail]) :-
+    Head = b,
     all_b(Tail).
 
-create_b_list(0, List).
-
-create_b_list(Len, List):-
-    Len > 0,
-    Len1 is Len - 1,
-    List1 is [b | List],
-    create_b_list(Len1, List1).
-
+% Calculez L1 = length(X), L2 = length(Y), verific daca sunt egale si dupa verific daca X contine doar a si Y contine doar b
+% De remarcat ca daca X sau Y sunt nule, modul in care sunt scrise all_a si all_b le face liste de lungimea potrivita de a, respectiv b
 trans_a_b(X, Y) :-
-    all_a(X),
-    all_b(Y),
     length(X, L1),
     length(Y, L2),
-    L1 == L2.
-
-trans_a_b(X, Y) :-
+    L1 == L2,
     all_a(X),
-    length(X, L1),
-    length(Y, L2),
-    L2 == 0,
-    create_b_list(L1, Y).
+    all_b(Y).
+
+% Cand am ajuns la lista vida la parametrii 2 si 3, inseamna ca am terminat procesul
+scalarMult(X, [], []).
+
+% Consider L1 = lista initiala si L2 = lista finala
+scalarMult(X, [Head | Tail], [NewHead | NewTail]) :-
+    NewHead is X * Head, % practic fac L2[i] = L1[i] * X
+    scalarMult(X, Tail, NewTail). % Continui asta pt pozitiile i + 1, ...
+
+% Pentru lista vida, produsul scalar e 0
+dot([], [], 0).
+
+% Plec de la premiza ca lungimile celor 2 liste sunt egale
+dot([H1 | T1], [H2 | T2], Result) :-
+    dot(T1, T2, NewRes), % Calculez produsul scalar dintre Tail1 si Tail2
+    Result is NewRes + H1 * H2. % Practic fac Result = produs_scalar(L1[i + 1:], L2[i + 1:]) + L1[i] * L2[i] (am folosit notatii python)
+
+% Daca X > Y, maximul (stocat in VAL) este X.
+maxim(X, Y, VAL) :-
+    X > Y,
+    VAL is X.
+
+% Altfel, maximul e Y.
+maxim(X, Y, VAL) :-
+    Y >= X,
+    VAL is Y.
+
+% Maximul listei vide de nr naturale e 0
+max([], 0).
+
+max([Head | Tail], Result) :-
+    max(Tail, NewRes), % NewRes = maximul pe sublista Tail = L[i + 1:]
+    maxim(Head, NewRes, Result). % Result = max(Head, newRes)
